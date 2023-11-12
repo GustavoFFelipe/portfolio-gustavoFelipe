4dd0e43a225e7aa5c0693fbc77a80c25de5f3fcc
Gustavo Felipe <gustavo.ffelipe@gmail.com>
2023-11-12 15:39:36 -0300

    sidebar complete and about section in main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,47 @@
 import './Main.scss';
 
-export default function Main(){
-    return (
-        <>
-            <div id="main">
-              <div id="about">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae aliquid tempora blanditiis veritatis earum eveniet accusantium temporibus architecto vel itaque assumenda fuga reiciendis eum mollitia, est atque perferendis! Tenetur, a!
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae aliquid tempora blanditiis veritatis earum eveniet accusantium temporibus architecto vel itaque assumenda fuga reiciendis eum mollitia, est atque perferendis! Tenetur, a!
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae aliquid tempora blanditiis veritatis earum eveniet accusantium temporibus architecto vel itaque assumenda fuga reiciendis eum mollitia, est atque perferendis! Tenetur, a!
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae aliquid tempora blanditiis veritatis earum eveniet accusantium temporibus architecto vel itaque assumenda fuga reiciendis eum mollitia, est atque perferendis! Tenetur, a!
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae aliquid tempora blanditiis veritatis earum eveniet accusantium temporibus architecto vel itaque assumenda fuga reiciendis eum mollitia, est atque perferendis! Tenetur, a!
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae aliquid tempora blanditiis veritatis earum eveniet accusantium temporibus architecto vel itaque assumenda fuga reiciendis eum mollitia, est atque perferendis! Tenetur, a!
-                </p>
-              </div>
-              <div id="projects">
-                <div className="project-container"></div>
-                <div className="project-container"></div>
-                <div className="project-container"></div>
-              </div>
-            </div>
-        </>
-    )
+export default function Main() {
+  return (
+    <>
+      <div id="main">
+        <div id="about">
+          <p>
+              Hi, I'm Gustavo a front end developer from Brazil, I'm studying a bachelor's degree in computer science, and in addition to web development that as my principal field, I'm also a studying postgraduated in Software engineer.
+            </p>
+            <br />
+            <p>
+              Among my hardskills I usually work with a few frameworks and technologies as
+              reactjs,
+              and now I learning Typescript and nodeJs to build more complex applications.
+            </p>
+            <p>
+              I already have work experience where I was able to develop many of my softskills such as teamwork,
+               communication,
+                 leadership,
+                 time management
+                  and quality control during the creation processes.
+            </p>
+            <br />
+            <p>
+               All this was possible because I have to work with many teams from different sectors so I always need to interact and talk to others peoples.
+            </p>
+            <p>
+               I am sure that I can be the best candidate for this opportunity, because I learn quickly, adapt easily to changes, I love learning new things and working in different parts of a project.
+            </p>
+            <p>
+               I'm determined person in my goals and always work hard to overcome challenges. So I believe that I can do too much as part of your team.
+            </p>
+            <br />
+            <p>
+               Well that's a little about me and I hope to meet you very soon.  Goodbye
+          </p>
+        </div>
+        <div id="projects">
+          <div className="project-container"></div>
+          <div className="project-container"></div>
+          <div className="project-container"></div>
+        </div>
+      </div>
+    </>
+  )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,36 @@
 import './Sidebar.scss'
+import instagram from '../assets/images/icons/instagram.png';
+import github from '../assets/images/icons/github.png';
+import linkedin from '../assets/images/icons/linkedin.png';
 
 export default function Sidebar(){
     return (
         <>
             <div id="sidebar">
-            <h1>Gustavo Felipe</h1>
-            <span>Desenvolvedor Front-end</span>
-            <p>Resumo de habilidades na criação de um site</p>
-            <nav className="menu">
-              <ul>
-                <li><a href="#about">Sobre</a></li>
-                <li><a href="#projects">Projetos</a></li>
-                <li><a href="contact">Contato</a></li>
-              </ul>
-            </nav>
+            <div className="container">
+{/*---------------------Title from sidebar------------- */}
+              <div className="title">
+                <h1 id='name-title'>Gustavo Felipe</h1>
+                <span id='job-title'>Desenvolvedor Front-end</span>
+                <p id='sub-title'>Trazendo praticidade, criatividade e eficacia ao seu dia a dia na web.</p>
+              </div>
+    {/*--------------Navigation menu -------------*/}
+              <nav className="menu">
+                <ul>
+                  <li><a id='about-link' href="#about">Sobre</a></li>
+                  <li><a href="#projects">Projetos</a></li>
+                  <li><a href="#contact">Contato</a></li>
+                </ul>
+              </nav>
+{/*-------------Social menu icons --------------*/}
+              <div id="social">
+                <ul>
+                  <li><a href="https:instagram.com/gu.ffelipe"><img src={instagram} alt="" /></a></li>
+                  <li><a href="https://github.com/GustavoFFelipe"><img src={github} alt="" /></a></li>
+                  <li><a href="https:linkedin.com/in/gustavoffelipe"><img src={linkedin} alt="" /></a></li>
+                </ul>
+              </div>
+            </div>
           </div>
         </>
     )
